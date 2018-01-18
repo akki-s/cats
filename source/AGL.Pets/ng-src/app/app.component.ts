3f1309e3a2a85c873cf2ebf9b0b8a60d64a0261a
aakash <3936081+akki-s@users.noreply.github.com>
2018-01-18 14:14:54 +1100

    display cats on UI using angular

--- source/AGL.Pets/ng-src/app/app.component.ts
+++ source/AGL.Pets/ng-src/app/app.component.ts
@@ -12,14 +12,20 @@ export class AppComponent implements OnInit {
   title: string = 'AGL Coding Test';
   petsResult: PetsGrouped[] = [];
   message: string = '';
+  isLoading: boolean = true;
   ngOnInit(): void {
     this._httpService.get('/api/pets').subscribe(response => {
-      console.log('api result', response.status);
+      console.log('api result', response.status, response.statusText);
       if (response.status == 200)
+      {
         this.petsResult = response.json();
+        if (this.petsResult.length == 0)
+          this.message = 'No data returned from Api';
+      }
       else
-        this.message = response.statusText;
+        this.message = response.statusText + ' - There is an issue with Api.';
       console.log('GROUPED', this.petsResult);
+      this.isLoading = false;
     }); 
   }
 }
